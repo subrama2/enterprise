ff08bc03b1f3c38c03d0e4c89771c104e8453d1a
annamalai subramanian <subrama2@mail.uc.edu>
2023-09-30 16:15:01 -0400

    Completed individual assignment. added controller,dao,dto, service, updated html site

---
+++ src/main/java/com/myplantdiary/enterprise/dto/Specimen.java
@@ -0,0 +1,12 @@
+package com.myplantdiary.enterprise.dto;
+
+import lombok.Data;
+
+public @Data class Specimen {
+        private int PlantId;
+        private String specimenId;
+        private String latitude;
+        private String longitude;
+        private String description;
+    }
+//Add try catch by save to get the results saved in you iphone camera and you should be able to sube this comment after you finished t
